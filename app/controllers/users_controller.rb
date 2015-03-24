9e495f486be6779c08e988d18fb34b7c69bd6abb
Kevin <supergoodblog@gmail.com>
2015-03-23 19:42:54 -0500

    added #edit and #update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,37 @@
 class UsersController < ApplicationController
 	def index
-		@user = User.all
+	    @users = User.all
 	end
-
+	  
 	def new
+    	@user = User.new
+	end
+	
+	def create
+	    @user = User.new(user_params)
+	    # @user = User.new(:username => params[:username], :email => params[:email])
+	    if @user.save
+	      redirect_to root_path
+	    else
+	      render :new
+	    end
+  	end
 
+	def edit
+		@user = User.find(params[:id])
 	end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
 			redirect_to root_path
 		else
-			render :new
+			render :edit
 		end
 	end
 
 	private
 	def user_params
-		params.require(:user).permit(:name, :email)
+		params.require(:user).permit(:username, :email)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-root 'users#index'
+  
+  resources :users, only: [:new, :create, :edit, :update]
+
+  # You can have the root of your site routed with "root"
+  root 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
